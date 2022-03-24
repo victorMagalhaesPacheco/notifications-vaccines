61b567eaf55deda349e58853bb19004a8acabbdd
Victor Magalhaes <victor.magalhaesp@gmail.com>
2022-03-24 16:22:43 -0300

    add favicon

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <title>{{ Config::get('app.name') }}</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <link rel="stylesheet" href="{{ asset('site/spectral/assets/css/main.css') }}" />
